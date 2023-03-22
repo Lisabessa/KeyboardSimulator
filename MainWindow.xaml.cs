3f069172b9d4b883653bbfcda12a3c0d1d2a7037
Lisabessa <75796619+Lisabessa@users.noreply.github.com>
2023-03-22 23:57:31 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,473 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyboardApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool UpperCase;
+        bool ShiftCase;
+        Trainer trainer;
+        int ind;
+        System.Windows.Threading.DispatcherTimer timer;
+        int Chars = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            StopBtn.IsEnabled = false;
+            StartBtn.IsEnabled = true;
+            GenText.Text = "";
+            UserText.Text = "";
+            UpperCase = false;
+            ShiftCase = false;
+            trainer = new Trainer();
+            Difficulty.Value = 3;
+            Difficulty.Minimum = 3;
+            ind = 0;
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!StartBtn.IsEnabled)
+            {
+                if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
+                {
+                    PanelDefault.Visibility = Visibility.Collapsed;
+                    PanelCaps.Visibility = Visibility.Collapsed;
+                    PanelShift.Visibility = Visibility.Visible;
+                    UpperCase = true;
+                    ShiftCase = true;
+
+                }
+                else if (e.Key == Key.CapsLock)
+                {
+                    if (UpperCase)
+                    {
+                        PanelCaps.Visibility = Visibility.Collapsed;
+                        PanelShift.Visibility = Visibility.Collapsed;
+                        PanelDefault.Visibility = Visibility.Visible;
+                        UpperCase = false;
+                        ShiftCase = false;
+                    }
+                    else
+                    {
+                        PanelDefault.Visibility = Visibility.Collapsed;
+                        PanelShift.Visibility = Visibility.Collapsed;
+                        PanelCaps.Visibility = Visibility.Visible;
+                        UpperCase = true;
+                        ShiftCase = false;
+                    }
+
+                }
+                else if (e.Key == Key.Space)
+                {
+                    UserText.Text += " ";
+                    Count(" ");
+                }
+                else if (e.Key == Key.LeftAlt || e.Key == Key.RightAlt || e.Key == Key.LWin
+                    || e.Key == Key.RWin || e.Key == Key.LeftCtrl || e.Key == Key.RightCtrl
+                    || e.Key == Key.Enter || e.Key == Key.Back || e.Key == Key.Tab)
+                {
+                    //ничего не происходит
+                }
+                else if (e.Key == Key.Oem3)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "~";
+                        Count("~");
+                    }
+                    else
+                    {
+                        UserText.Text += "`";
+                        Count("`");
+                    }
+                }
+                else if (e.Key == Key.D1)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "!";
+                        Count("!");
+                    }
+                    else
+                    {
+                        UserText.Text += "1";
+                        Count("1");
+                    }
+
+                }
+                else if (e.Key == Key.D2)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "@";
+                        Count("@");
+                    }
+                    else
+                    {
+                        UserText.Text += "2";
+                        Count("2");
+                    }
+                }
+                else if (e.Key == Key.D3)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "#";
+                        Count("#");
+                    }
+                    else
+                    {
+                        UserText.Text += "3";
+                        Count("3");
+                    }
+                }
+                else if (e.Key == Key.D4)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "$";
+                        Count("$");
+                    }
+                    else
+                    {
+                        UserText.Text += "4";
+                        Count("4");
+                    }
+                }
+                else if (e.Key == Key.D5)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "%";
+                        Count("%");
+                    }
+                    else
+                    {
+                        UserText.Text += "5";
+                        Count("5");
+                    }
+                }
+                else if (e.Key == Key.D6)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "^";
+                        Count("^");
+                    }
+                    else
+                    {
+                        UserText.Text += "6";
+                        Count("6");
+                    }
+                }
+                else if (e.Key == Key.D7)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "&";
+                        Count("&");
+                    }
+                    else
+                    {
+                        UserText.Text += "7";
+                        Count("7");
+                    }
+                }
+                else if (e.Key == Key.D8)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "*";
+                        Count("*");
+
+                    }
+                    else
+                    {
+                        UserText.Text += "8";
+                        Count("8");
+                    }
+                }
+                else if (e.Key == Key.D9)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "(";
+                        Count("(");
+                    }
+                    else
+                    {
+                        UserText.Text += "9";
+                        Count("9");
+                    }
+                }
+                else if (e.Key == Key.D0)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += ")";
+                        Count(")");
+                    }
+                    else
+                    {
+                        UserText.Text += "0";
+                        Count("0");
+                    }
+                }
+                else if (e.Key == Key.OemMinus)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "_";
+                        Count("_");
+                    }
+                    else
+                    {
+                        UserText.Text += "-";
+                        Count("-");
+                    }
+                }
+                else if (e.Key == Key.OemPlus)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "+";
+                        Count("+");
+                    }
+                    else
+                    {
+                        UserText.Text += "=";
+                        Count("=");
+                    }
+                }
+                else if (e.Key == Key.OemOpenBrackets)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "{";
+                        Count("{");
+                    }
+                    else
+                    {
+                        UserText.Text += "[";
+                        Count("[");
+                    }
+                }
+                else if (e.Key == Key.Oem6)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "}";
+                        Count("}");
+                    }
+                    else
+                    {
+                        UserText.Text += "]";
+                        Count("]");
+                    }
+                }
+                else if (e.Key == Key.Oem1)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += ":";
+                        Count(":");
+                    }
+                    else
+                    {
+                        UserText.Text += ";";
+                        Count(";");
+                    }
+                }
+                else if (e.Key == Key.OemQuotes)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += '"';
+                        Count("\"");
+                    }
+                    else
+                    {
+                        UserText.Text += "'";
+                        Count("'");
+                    }
+                }
+                else if (e.Key == Key.Oem5)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "|";
+                        Count("|");
+                    }
+                    else
+                    {
+                        UserText.Text += "\\";
+                        Count("\\");
+                    }
+                }
+                else if (e.Key == Key.OemPeriod)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += ">";
+                        Count(">");
+                    }
+                    else
+                    {
+                        UserText.Text += ".";
+                        Count(".");
+                    }
+                }
+                else if (e.Key == Key.OemQuestion)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "?";
+                        Count("?");
+                    }
+                    else
+                    {
+                        UserText.Text += "/";
+                        Count("/");
+                    }
+                }
+                else if (e.Key == Key.OemComma)
+                {
+                    if (ShiftCase)
+                    {
+                        UserText.Text += "<";
+                        Count("<");
+                    }
+                    else
+                    {
+                        UserText.Text += ",";
+                        Count(",");
+                    }
+
+                }
+                else if (!UpperCase)
+                {     
+                    UserText.Text += e.Key.ToString().ToLower();
+                    Count(e.Key.ToString().ToLower());
+
+                }
+                else if (UpperCase)
+                {   
+                    UserText.Text += e.Key.ToString().ToUpper();
+                    Count(e.Key.ToString().ToUpper());
+                }
+            }
+             
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!StartBtn.IsEnabled)
+            {
+                if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
+                {
+                    PanelShift.Visibility = Visibility.Collapsed;
+                    PanelCaps.Visibility = Visibility.Collapsed;
+                    PanelDefault.Visibility = Visibility.Visible;
+                    UpperCase = false;
+                    ShiftCase = false;
+                }
+            }       
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ind = 0;
+            GenText.Text = "";
+            UserText.Text = "";
+            StopBtn.IsEnabled = true;
+            StartBtn.IsEnabled = false;
+            trainer.Errors = 0;
+            Fails.Text = trainer.Errors.ToString();
+            GenText.Text = trainer.GenTest();
+
+            timer = new System.Windows.Threading.DispatcherTimer();
+
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 10);
+            timer.Start();
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            EndTheGame();
+        }
+        private void timerTick(object sender, EventArgs e)
+        {
+            trainer.Speed = Chars * 6;
+            CharsMin.Text = trainer.Speed.ToString();
+            Chars = 0;
+        }
+
+        private void Dificulty_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {   
+            trainer.Level = (int)Difficulty.Value;
+            DifficultyInfo.Text = trainer.Level.ToString();
+        }
+
+        private void CaseSensitiveInfo_Checked(object sender, RoutedEventArgs e)
+        {
+            trainer.CaseSensitive = true;
+        }
+
+        private void CaseSensitiveInfo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            trainer.CaseSensitive = false;
+        }
+
+        private void Count(string sym)
+        {
+            Chars++;
+
+            if (!trainer.Comparison(ind, sym))
+            {
+                trainer.Errors ++;
+                Fails.Text = trainer.Errors.ToString();
+            }
+            
+            ind++;
+            if(ind == 50)
+            {            
+                MessageBox.Show(trainer.GameResult());
+                EndTheGame();
+            }
+        }
+
+        private void EndTheGame()
+        {
+            timer.Stop();
+            ind = 0;
+            StopBtn.IsEnabled = false;
+            StartBtn.IsEnabled = true;
+            trainer.Errors = 0;
+            Fails.Text = trainer.Errors.ToString();
+            GenText.Text = "";
+            UserText.Text = "";
+            trainer.Speed = 0;
+            CharsMin.Text = trainer.Speed.ToString();
+            Chars = 0;
+        }
+    }
+}
---
+++ Trainer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyboardApp
+{
+    class Trainer
+    {
+        public int Speed = 0;
+        public bool CaseSensitive { get; set; }
+        public int Level { get; set; }
+        public int Errors { get; set; }
+        public string Test;
+
+        private string alphabetCS = "`1234567890-=~!@#$%^&*()_+qwertyuiop[]QWERTYUIOP{}asdfghjkl;'\\ASDFGHJKL:\"|zxcvbnm,./ZXCVBNM<>?";
+        private string alphabet = "`1234567890-=qwertyuiop[]asdfghjkl;'\\zxcvbnm,./";
+
+        public Trainer()
+        {
+            CaseSensitive = false;
+            Level = 3;
+        }
+
+        public string GenTest() // метод генерации строки
+        {
+            string test = "";
+            string currAlpha = "";
+            List<int> indexes = new List<int>();
+            int num;
+            Random rand = new Random();
+            for(int i=0; i<this.Level; i++)
+            {
+                if (CaseSensitive)
+                {
+                    num = rand.Next(alphabetCS.Length);
+                    while (indexes.Contains(num))
+                    {
+                        num = rand.Next(alphabetCS.Length);
+                    }
+                    indexes.Add(num);
+                    currAlpha += alphabetCS[num];
+                }
+                else
+                {
+                    num = rand.Next(alphabet.Length);
+                    while (indexes.Contains(num))
+                    {
+                        num = rand.Next(alphabet.Length);
+                    }
+                    indexes.Add(num);
+                    currAlpha += alphabet[num];
+                }
+            }
+            currAlpha += " ";
+            
+            for(int i=0; i<50; i++)
+            {
+                num = rand.Next(currAlpha.Length);
+                test += currAlpha[num];
+            }
+
+            this.Test = test;
+            return test;
+        }
+
+        public bool Comparison(int ind, string symb)
+        {
+            if(this.Test[ind] == symb[0])
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string GameResult()
+        {
+            string res = "Поздравляем! Вы закончили игру\n";
+            res += "Вы допустили " + this.Errors.ToString() + " ошибок.\n";
+
+            if(this.Errors <= 3)
+            {
+                res += "Отличный результат, так держать!\n";
+            }
+            else if(this.Errors <= 5)
+            {
+                res += "Ты молодец!\n";
+            }
+            else if (this.Errors <= 10)
+            {
+                res += "Неплохо, но будет лучше!\n";
+            }
+            else
+            {
+                res += "В следующий раз всё точно получится!\n";
+            }
+
+
+            return res;
+        }
+    }
+}
